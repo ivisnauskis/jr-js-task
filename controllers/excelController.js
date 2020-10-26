e51ea10d6f2ad5624eb165220f058bdcf839598c
ivisnauskis <invisnauskis@gmail.com>
2020-10-26 11:54:06 +0200

    async controller, immediately-invoked function removed.

--- controllers/excelController.js
+++ controllers/excelController.js
@@ -3,18 +3,16 @@ const { getXlsxStream } = require("xlstream");
 
 var upload = require("../multer");
 
-excelRouter.post("/", upload.single("data"), (req, res) => {
+excelRouter.post("/", upload.single("data"), async (req, res) => {
   const file = req.file;
   if (file.originalname.toLowerCase().endsWith(".xlsx")) {
     let result = 0;
-    (async () => {
-      const stream = await getXlsxStream({
-        filePath: file.path,
-        sheet: 0,
-      });
-      stream.on("data", (x) => (result += x.raw.obj.A));
-      stream.on("end", () => res.send(`SUM is ${result}`));
-    })();
+    const stream = await getXlsxStream({
+      filePath: file.path,
+      sheet: 0,
+    });
+    stream.on("data", (x) => (result += x.raw.obj.A));
+    stream.on("end", () => res.send(`SUM is ${result}`));
   } else {
     res.status(400).send("Bad file format");
   }
